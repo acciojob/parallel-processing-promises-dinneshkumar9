444a78b29969d695809822a6ef30a3c68e16ec49
Dinesh kumar <dinneshkumar9@gmail.com>
2023-04-21 10:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,28 +12,23 @@ const images = [
     { url: 'https://picsum.photos/id/10/200/300' },
   ];
   
-  function downloadImages() {
-    const promises = images.map(image => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-        img.src = image.url;
+  const output = document.querySelector("#output");
+const downloadBtn = document.querySelector("#download-images-button");
+
+async function downloadPic() {
+  try {
+    for (const image of images) {
+      const img = await new Promise((resolve, reject) => {
+        const imgElement = new Image();
+        imgElement.onload = () => resolve(imgElement);
+        imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+        imgElement.src = image.url;
       });
-    });
-  
-    Promise.all(promises)
-      .then(images => {
-        const outputDiv = document.getElementById('output');
-        images.forEach(image => {
-          outputDiv.appendChild(image);
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      });
+      output.appendChild(img);
+    }
+  } catch (error) {
+    console.error(error);
   }
-  
-  const downloadButton = document.getElementById('download-images-button');
-  downloadButton.addEventListener('click', downloadImages);
-  
+}
+
+downloadBtn.addEventListener("click", downloadPic);
